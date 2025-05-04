52d6e5b6197b7d5eed177f2ebd98ce388eea1dc4
minixjee4 <jb806359@gmail.com>
2025-05-04 22:44:38 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,15 +7,15 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
     // ===== BOT CORE SETTINGS =====
-    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~h9RwSJYT#Nt2E6P_tf1MBMOuLYpDzrev9vyJ-kDMmlfggxxlEKXs",  // Your bot's session ID (keep it secure)
+    SESSION_ID: process.env.SESSION_ID || "SUBZERO-MD~VpEns6DiN4u2bUotNppl",  // Your bot's session ID (keep it secure)
     PREFIX: process.env.PREFIX || ".",         // Command prefix (e.g., "., / ! * - +")
-    BOT_NAME: process.env.BOT_NAME || "♥️𝐒𝐇𝐀𝐇𝐈𝐃 𝐊𝐈𝐍𝐆♥️",  // Bot's display name
+    BOT_NAME: process.env.BOT_NAME || "♥️MinixJee♥️",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     REPO: process.env.REPO || "https://github.com/mrfrankofcc/SUBZERO-MD",  // Bot's GitHub repo
 
     // ===== OWNER & DEVELOPER SETTINGS =====
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "923403747874",  // Owner's WhatsApp number
-    OWNER_NAME: process.env.OWNER_NAME || "♥️𝐒𝐇𝐀𝐇𝐈𝐃 𝐊𝐈𝐍𝐆♥️",           // Owner's name
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "2348164949238",  // Owner's WhatsApp number
+    OWNER_NAME: process.env.OWNER_NAME || "♥️Minixjee♥️",           // Owner's name
     DEV: process.env.DEV || "263719647303",                     // Developer's contact number
     DEVELOPER_NUMBER: '263719647303@s.whatsapp.net',            // Developer's WhatsApp ID
 
